d70479f646d2107d51df05eeed89f0710151c279
Pierre Carrier <pierre@meteor.com>
2016-08-05 16:12:22 -0700

    leak fast

--- bundle/main.js
+++ bundle/main.js
@@ -3,7 +3,7 @@ setInterval(function () {
   var s = "", n = Math.random();
   for (i = 0; i < 100; i++) s = s + n;
   leak.push(s);
-}, 10);
+}, 1);
 
 require('http').createServer(function (req, rep) {
   rep.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
